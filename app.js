224e7aa478292f9bdaf33e4ecbf4216c048c9201
Nihad Nahmatli <143920615+bakueduaz@users.noreply.github.com>
2024-08-31 12:40:12 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = `https://key-practise.vercel.app/api/news`
+            let url = `https://apidemo-jade.vercel.app/api/news`
             async function myweather() {
                 let responce = await fetch(url)
                 let data = await responce.json()
